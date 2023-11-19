ec186f28ce5e146d10202d2b657ec80be29ed37d
adrian-antonic <116621818+adrian-antonic@users.noreply.github.com>
2023-11-19 11:47:23 +0100

    company administrator initiated

---
+++ ISA-project/src/main/java/com/example/ISAproject/repository/CompanyAdministratorRepository.java
@@ -0,0 +1,9 @@
+package com.example.ISAproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ISAproject.model.CompanyAdministrator;
+
+public interface CompanyAdministratorRepository extends JpaRepository<CompanyAdministrator, Long>{
+
+}
