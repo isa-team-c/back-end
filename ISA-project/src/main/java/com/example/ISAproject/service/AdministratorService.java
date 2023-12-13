e0def9bb125a8e8cfb3fd81e964bf68bd1162732
adrian-antonic <116621818+adrian-antonic@users.noreply.github.com>
2023-12-13 16:04:48 +0100

    creating administrator done

---
+++ ISA-project/src/main/java/com/example/ISAproject/service/AdministratorService.java
@@ -0,0 +1,44 @@
+package com.example.ISAproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ISAproject.dto.AdministratorDto;
+import com.example.ISAproject.dto.CompanyAdministratorDto;
+import com.example.ISAproject.dto.RegularUserDto;
+import com.example.ISAproject.dto.UserDto;
+import com.example.ISAproject.model.Administrator;
+import com.example.ISAproject.model.CompanyAdministrator;
+import com.example.ISAproject.model.RegularUser;
+import com.example.ISAproject.repository.AdministratorRepository;
+
+@Service
+public class AdministratorService {
+	@Autowired
+	private AdministratorRepository administratorRepository;
+	
+	@Autowired
+    private UserService userService;
+	
+	public Administrator createAdministrator(AdministratorDto newAdminDto) {
+    	
+    	Administrator newAdmin = new Administrator();
+        
+        com.example.ISAproject.model.User newUser = new com.example.ISAproject.model.User();
+        newUser.setEmail(newAdminDto.getUser().getEmail());
+        newUser.setPassword(newAdminDto.getUser().getPassword());
+        newUser.setName(newAdminDto.getUser().getName());
+        newUser.setSurname(newAdminDto.getUser().getSurname());
+        newUser.setCity(newAdminDto.getUser().getCity());
+        newUser.setCountry(newAdminDto.getUser().getCountry());
+        newUser.setPhoneNumber(newAdminDto.getUser().getPhoneNumber());
+        newUser.setProfession(newAdminDto.getUser().getProfession());
+        newUser.setCompanyInformation(newAdminDto.getUser().getCompanyInformation());
+        newUser.setRole(newAdminDto.getUser().getRole());
+        newUser.setIsVerified(true);
+        
+        newAdmin.setUser(newUser);
+        
+        return administratorRepository.save(newAdmin);
+    }
+}
