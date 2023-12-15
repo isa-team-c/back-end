4df2fe8caa7bd29f3ef2ad0e70fb01a4f9274a94
ayadjo <andjela1108@gmail.com>
2023-12-15 18:46:40 +0100

    data-postrgres.sql modified

--- ISA-project/src/main/resources/data-postgres.sql
+++ ISA-project/src/main/resources/data-postgres.sql
@@ -8,11 +8,6 @@ INSERT INTO public.users(city, company_information, country, email, is_verified,
 INSERT INTO public.users(city, company_information, country, email, is_verified, last_password_reset_date, name, password, phone_number, profession, surname, role_id)
 	VALUES ('Cacak', 'preduzece1', 'Srbija', 'admin@gmail.com', true, null, 'Isidora', '$2a$10$lhYgvOwC1Q.fxzQBkwVNI.xqwKaoQiY6Gum5fzeN9jsuYStzORNGi', '065728314', 'softverski inzenjer', 'Milosevic',2);
 
-INSERT INTO public.regular_user(
-	user_id, penalties)
-	VALUES (1, 0);
-
-
 INSERT INTO public.company(address, average_rating, description, name)
 	VALUES ('Vuka Karadzica', 5, 'odlicno', 'company1');
 INSERT INTO public.company(address, average_rating, description, name)
@@ -21,7 +16,7 @@ INSERT INTO public.company(address, average_rating, description, name)
 INSERT INTO public.equipment(description, name, quantity, type)
 	VALUES ('medicinska', 'Stethoscope', 5, 'Medical Device');
 INSERT INTO public.equipment (description,name,quantity,type) 
-	VALUES ('računarska oprema','Laptop',10,'IT Equipment');
+	VALUES ('raÄunarska oprema','Laptop',10,'IT Equipment');
 INSERT INTO public.equipment (description,name,quantity,type) 
 	VALUES ('laboratorijska oprema','Microscope',3,'Laboratory Equipment');
 
@@ -35,6 +30,9 @@ INSERT INTO public.company_equipment(
 	company_id, equipment_id)
 	VALUES (2, 3);
 
+INSERT INTO public.company_administrator(
+	user_id, company_id)
+	VALUES (2, 1);
 
 INSERT INTO public.appointments(
 	id, duration, is_free, start_date, administrator_id)
@@ -51,7 +49,3 @@ INSERT INTO public.company_appointment(
 INSERT INTO public.company_appointment(
 	company_id, appointment_id)
 	VALUES (1, 2);
-
-INSERT INTO public.company_administrator(
-	user_id, company_id)
-	VALUES (2, 1);
