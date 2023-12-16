3c5f134011ff17d4d28d611e1387d53f68b2c9fa
adrian-antonic <116621818+adrian-antonic@users.noreply.github.com>
2023-12-16 12:00:21 +0100

    sql script fix

--- ISA-project/src/main/resources/data-postgres.sql
+++ ISA-project/src/main/resources/data-postgres.sql
@@ -10,7 +10,7 @@ INSERT INTO public.users(city, company_information, country, email, is_verified,
 INSERT INTO public.users(city, company_information, country, email, is_verified, last_password_reset_date, name, password, phone_number, profession, surname, role_id)
 	VALUES ('Cacak', 'preduzece1', 'Srbija', 'admin@gmail.com', true, null, 'Isidora', '$2a$10$lhYgvOwC1Q.fxzQBkwVNI.xqwKaoQiY6Gum5fzeN9jsuYStzORNGi', '065728314', 'softverski inzenjer', 'Milosevic',2);
 INSERT INTO public.users(city, company_information, country, email, is_verified, last_password_reset_date, name, password, phone_number, profession, surname, role_id)
-	VALUES ('Grad', '/', 'Drzava', 'admin@gmail.com', true, null, 'Admin', '$2a$10$E17mSVuCMgS9jfc/WZcK8eFrjD/uYgZ1XcAd2T55rwZOKv7ki23tW', '1234567890', 'profesija', 'Adminovic', 3);
+	VALUES ('Grad', '/', 'Drzava', 'adminn@gmail.com', true, null, 'Admin', '$2a$10$E17mSVuCMgS9jfc/WZcK8eFrjD/uYgZ1XcAd2T55rwZOKv7ki23tW', '1234567890', 'profesija', 'Adminovic', 3);
 
 
 INSERT INTO public.company(address, average_rating, description, name)
